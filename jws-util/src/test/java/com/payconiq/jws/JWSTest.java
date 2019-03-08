4524f95d92703b0223516994ad48cec3bad63335
payconiq-devsupport <48285050+payconiq-devsupport@users.noreply.github.com>
2019-03-08 13:09:47 +0100

    Update JWSTest
    Corrected ext1 to ext

--- jws-util/src/test/java/com/payconiq/jws/JWSTest.java
+++ jws-util/src/test/java/com/payconiq/jws/JWSTest.java
@@ -16,7 +16,7 @@ public class JWSTest {
     public void validateSignature() throws Exception {
         String body = "{\"paymentId\":\"687e0f4c373dbc4f381d31cd\",\"transferAmount\":700,\"tippingAmount\":0,\"amount\":700,\"totalAmount\":700,\"description\":\"stT\",\"reference\":\"Order1234\",\"createdAt\":\"2019-03-07T10:24:05.127Z\",\"expireAt\":\"2019-03-07T10:27:05.127Z\",\"status\":\"SUCCEEDED\",\"debtor\":{\"name\":\"Ivan\",\"iban\":\"***********42901\"},\"currency\":\"EUR\"}";
         String jws = "eyJ0eXAiOiJKT1NFK0pTT04iLCJraWQiOiJlcy5zaWduYXR1cmUuZXh0LnBheWNvbmlxLmNvbSIsImFsZyI6IkVTMjU2IiwiaHR0cHM6Ly9wYXljb25pcS5jb20vaWF0IjoiMjAxOS0wMy0wN1QxMDoyNDoyMy44ODBaIiwiaHR0cHM6Ly9wYXljb25pcS5jb20vanRpIjoiODE1MjgxZjczM2VjMTRmIiwiaHR0cHM6Ly9wYXljb25pcS5jb20vcGF0aCI6Imh0dHBzOi8vd2ViaG9vay5zaXRlLzI5ZGYwZjBmLTE2MDctNDUxYy1hMjU2LTM3NDI4YTQwM2U4YSIsImh0dHBzOi8vcGF5Y29uaXEuY29tL2lzcyI6IlBheWNvbmlxIiwiaHR0cHM6Ly9wYXljb25pcS5jb20vc3ViIjoiNWJiMzcyODRlMzVlMmIyOWUzNjNkZjIyIiwiY3JpdCI6WyJodHRwczovL3BheWNvbmlxLmNvbS9pYXQiLCJodHRwczovL3BheWNvbmlxLmNvbS9qdGkiLCJodHRwczovL3BheWNvbmlxLmNvbS9wYXRoIiwiaHR0cHM6Ly9wYXljb25pcS5jb20vaXNzIiwiaHR0cHM6Ly9wYXljb25pcS5jb20vc3ViIl19..5SvAcU4hGMoyCvbiazvNz7d3Ciwc3hRHjF1x14mPiEAVA1DDl3GZEDUGpBp7zns8pDzCYZHGaYlTQM2ABUABag";
-        String environment = "ext1";
+        String environment = "ext";
         boolean result = ValidateJWS.validateJWSSignature(jws, body, environment);
 
         assertThat(result).as("Verify jws for v3 payment callback").isTrue();
